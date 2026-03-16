e242097f65838ff47401c54ee04835b46e4c84a3
pritvee <pritvee@users.noreply.github.com>
2026-03-16 22:29:44 +0530

    Fix Vercel SPA routing configuration and switch to BrowserRouter

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDom from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { AuthProvider } from './store/AuthContext';
 import { AppThemeProvider } from './store/ThemeContext';
@@ -10,7 +10,7 @@ import { CartProvider } from './store/CartContext';
 
 ReactDom.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <HashRouter
+        <BrowserRouter
             future={{
                 v7_startTransition: true,
                 v7_relativeSplatPath: true
@@ -27,7 +27,7 @@ ReactDom.createRoot(document.getElementById('root')).render(
                     </ProductProvider>
                 </AuthProvider>
             </AppThemeProvider>
-        </HashRouter>
+        </BrowserRouter>
     </React.StrictMode>
 );
 
